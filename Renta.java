e5d39df6fc6351d33866bd2a2efa22723b92a2ae
BrianTex <brianxd.tx@gmail.com>
2023-04-24 17:58:47 -0600

    Añadiendo clase Renta

---
+++ Renta.java
@@ -0,0 +1,27 @@
+import ProyectoCodigo.objetosRentables.ObjetoRentable;
+
+public class Renta implements Rentable{
+    private int dia;
+    private int mes;
+    private int annio;
+    private ObjetoRentable objRentable;
+
+    public Renta(int di, int m, int ann){
+        dia=di;
+        mes=m;
+        annio=ann;
+    }
+    public void rentar(){
+
+    }
+    public void devolver(){
+
+    }
+    public ObjetoRentable getObjRentable(){
+
+    }
+    public void setObjRentable(ObjetoRentable objRentable){
+        
+    }
+
+}
